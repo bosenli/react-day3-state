d4c8950814b91435e1418146fd60e8cff777825c
bosen L <bosenli2013@hotmail.com>
2022-03-16 23:46:55 -0400

    added Message.js as react Hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Message from './components/Message';
 
+
+//https://reactjs.org/docs/hooks-intro.html
+//Props:
+// immutable (can't be changed);
+// State: not immutable (can be changed),
+// can be passed down into another component via props
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Message />
     </div>
   );
 }
---
+++ src/components/Message.js
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+
+
+const Message = () => {
+    const [message, setMessage] = useState("");
+    // message is what holds the current state
+    // setMessage is a function that changes the state
+    // useState is a hook. The argument ("" in this case) defines the inital value
+        // Initial value can be a string, null, true, false, an integer, an array, etc.
+
+    return (
+        <div>
+            <input 
+                type="text"
+                value={message}
+                placeholder="Enter a message" 
+                onChange={ (event) =>  setMessage(event.target.value) }
+                    // onChange is an event listener that listens for change
+                    // It requires a callback function
+                        // event.target.value
+                            // EVENT represents a change to the webpage
+                                // TARGET points to the specific change
+                                    // VALUE is the value that is in the input box
+                    
+            />
+
+            <p>{ message }</p>
+                {/* Displays the message in real time. */}
+
+        </div>
+    )
+}
+
+export default Message;
